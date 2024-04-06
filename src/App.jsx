edaa488fec0b56044e395612330d926d8c1ec62a
Raheem Quddus <84246065+Quddos@users.noreply.github.com>
2024-04-06 06:15:21 +0530

    creating a components for the Navbar and importing it into our app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Navbar from './components/Navbar';
 
 const App = () => {
   const name = 'Quddus';
@@ -13,46 +14,8 @@ const App = () => {
   // Returning more than one element 
   return (
     <>
-      <nav className="bg-indigo-700 border-b border-indigo-500">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="flex h-20 items-center justify-between">
-          <div
-            className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
-          >
-            {/* <!-- Logo --> */}
-            <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
-              <img
-                className="h-10 w-auto"
-                src="images/logo.png"
-                alt="React Jobs"
-              />
-              <span className="hidden md:block text-white text-2xl font-bold ml-2"
-                >React Jobs</span
-              >
-            </a>
-            <div className="md:ml-auto">
-              <div className="flex space-x-2">
-                <a
-                  href="/index.html"
-                  className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Home</a
-                >
-                <a
-                  href="/jobs.html"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Jobs</a
-                >
-                <a
-                  href="/add-job.html"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Job</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+      <Navbar/>
+     
 
     {/* <!-- Hero --> */}
     <section className="bg-indigo-700 py-20 mb-4">
