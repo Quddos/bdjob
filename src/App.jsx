865e24ada70b37273006d24a30e8d4a70716d6fa
Raheem Quddus <84246065+Quddos@users.noreply.github.com>
2024-04-06 05:43:08 +0530

    just basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
 const App = () => {
+  const name = 'Quddus';
+  const colors = ['blue', 'green', 'purple', ]
+  const ramadan = true
+
+
+
+
+
+
+  // Returning more than one element 
   return (
-    <div className='text-5xl'>Hello world</div>
+    <>
+      <div className='text-5xl'>Hello world</div>
+      <p>this is {name}</p>
+
+      <ul>
+        {colors.map((color, index)=>(
+          <li key={index}>{color}</li>
+        ))}
+      </ul>
+
+      {ramadan && <h1>Dear {name} happy Ramadan</h1>}
+    </>
   )
 }
 
