dd14eecb0feeade47ec45849a084880771f191dc
Raheem Quddus <84246065+Quddos@users.noreply.github.com>
2024-04-06 06:22:34 +0530

    adding logo to Navbar dynamic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import logo from '../assets/images/logo.png'
 
 const Navbar = () => {
   return (
@@ -13,7 +14,7 @@ const Navbar = () => {
             <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
               <img
                 className="h-10 w-auto"
-                src="images/logo.png"
+                src={logo}
                 alt="React Jobs"
               />
               <span className="hidden md:block text-white text-2xl font-bold ml-2"
