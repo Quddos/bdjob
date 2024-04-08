e82999b384320020b130b3cf8c3a3ca50e6accfb
Raheem Quddus <84246065+Quddos@users.noreply.github.com>
2024-04-08 05:33:39 +0530

    first state management implement on job description components

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,37 +1,47 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
-const JobListing = ( {job}) => {
+const JobListing = ({ job }) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = job.description;
+
+  if (!showFullDescription) {
+    description = description.substring(0, 90) + "...";
+  }
   return (
     <div className="bg-white rounded-xl shadow-md relative">
-    <div className="p-4">
-      <div className="mb-6">
-        <div className="text-gray-600 my-2">{job.type}</div>
-        <h3 className="text-xl font-bold">{job.title}</h3>
-      </div>
-
-      <div className="mb-5">
-      {job.description}
-      </div>
-
-      <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
-
-      <div className="border border-gray-100 mb-5"></div>
-
-      <div className="flex flex-col lg:flex-row justify-between mb-4">
-        <div className="text-orange-700 mb-3">
-          <i className="fa-solid fa-location-dot text-lg"></i>
-          {job.location}
+      <div className="p-4">
+        <div className="mb-6">
+          <div className="text-gray-600 my-2">{job.type}</div>
+          <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
-        <a
-          href={`/jobs/${job.id}`}
-          className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-        >
-        Read More
-        </a>
+
+        <div className="mb-5">{description}</div>
+
+        <button onClick={()=>setShowFullDescription((prevState)=>!prevState)} 
+        className='text-indigo-500 mb-5 hover:text-yellow-600'>
+            {showFullDescription? 'Less': 'More'}</button>
+
+        <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
+
+        <div className="border border-gray-100 mb-5"></div>
+
+        <div className="flex flex-col lg:flex-row justify-between mb-4">
+          <div className="text-orange-700 mb-3">
+            <i className="fa-solid fa-location-dot text-lg"></i>
+            {job.location}
+          </div>
+          <a
+            href={`/jobs/${job.id}`}
+            className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+          >
+            Read More
+          </a>
+        </div>
       </div>
     </div>
-</div>
-  )
-}
+  );
+};
 
-export default JobListing
+export default JobListing;
