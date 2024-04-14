9e635ee67cf14251a888573a5a2c86a1132b2077
Raheem Quddus <84246065+Quddos@users.noreply.github.com>
2024-04-15 02:41:32 +0530

    adding jobspage

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MainLayouts from "./layouts/MainLayouts";
+import JobsPage from "./pages/JobsPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayouts />}>
       <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
     </Route>
   )
 );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,19 +18,19 @@ const Navbar = () => {
               <div className="md:ml-auto">
                 <div className="flex space-x-2">
                   <a
-                    href="/index.html"
+                    href="/"
                     className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >
                     Home
                   </a>
                   <a
-                    href="/jobs.html"
+                    href="/jobs"
                     className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >
                     Jobs
                   </a>
                   <a
-                    href="/add-job.html"
+                    href="/add-job"
                     className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >
                     Add Job
