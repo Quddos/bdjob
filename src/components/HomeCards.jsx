d0f96f169c662711a0538acee041c52fb0f5fc86
Raheem Qudus <84246065+Quddos@users.noreply.github.com>
2024-04-18 21:15:48 +0530

    Link-router, replacing anchor tag with Link, to aid fast reload,

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "./Card";
+import {Link} from "react-router-dom";
 
 const HomeCards = () => {
   return (
@@ -12,12 +13,12 @@ const HomeCards = () => {
               <p className="mt-2 mb-4">
                 Browse our React jobs and start your career today
               </p>
-              <a
-                href="/jobs.html"
+              <Link
+                to="/jobs"
                 className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
               >
                 Browse Jobs
-              </a>
+              </Link>
             </Card>
 
             <Card bg="bg-indigo-100">
@@ -25,12 +26,12 @@ const HomeCards = () => {
               <p className="mt-2 mb-4">
                 List your job to find the perfect developer for the role
               </p>
-              <a
-                href="/add-job.html"
+              <Link
+                to="/add-job"
                 className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
               >
                 Add Job
-              </a>
+              </Link>
             </Card>
           </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../assets/images/logo.png";
+import { Link} from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -9,32 +10,32 @@ const Navbar = () => {
           <div className="flex h-20 items-center justify-between">
             <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
               {/* <!-- Logo --> */}
-              <a className="flex flex-shrink-0 items-center mr-4" href="/index">
+              <Link className="flex flex-shrink-0 items-center mr-4" to="/">
                 <img className="h-10 w-auto" src={logo} alt="React Jobs" />
                 <span className="hidden md:block text-white text-2xl font-bold ml-2">
                   Bdnaturetech
                 </span>
-              </a>
+              </Link>
               <div className="md:ml-auto">
                 <div className="flex space-x-2">
-                  <a
-                    href="/"
+                  <Link
+                    to="/"
                     className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >
                     Home
-                  </a>
-                  <a
-                    href="/jobs"
+                  </Link>
+                  <Link
+                    to="/jobs"
                     className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >
                     Jobs
-                  </a>
-                  <a
-                    href="/add-job"
+                  </Link>
+                  <Link
+                    to="/add-job"
                     className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >
                     Add Job
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
